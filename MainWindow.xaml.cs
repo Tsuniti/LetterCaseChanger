27580a1dec4b6705a5305694af0a4e50431996b9
Tsuniti <Tsuniti00@gmail.com>
2024-05-03 18:20:13 +0300

    Added notification

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using LetterCaseChanger.Controls;
 
 namespace LetterCaseChanger
 {
@@ -25,16 +26,34 @@ namespace LetterCaseChanger
         private void OnClick(object sender, RoutedEventArgs e)
         {
             if (TextBox.Text == string.Empty)
+            {
+                var notification = new Notification("The text field cannot be empty", Brushes.Red);
+                notification.Margin = new Thickness(5,-90,0,-100);
+                notification.Style = (Style)FindResource("Notification");
+                Grid.SetRow(notification, 4);
+                MyGrid.Children.Add(notification);
                 return;
+            }
 
             if (ToUpperCase.IsChecked == true)
             {
                 Clipboard.SetText(TextBox.Text.ToUpper());
+                var notification = new Notification("The text was successfully converted to uppercase and copied to the clipboard", Brushes.Lime);
+                notification.Margin = new Thickness(5,-90,0,-100);
+                notification.Style = (Style)FindResource("Notification");
+                Grid.SetRow(notification, 4);
+                MyGrid.Children.Add(notification);
                 return;
+                
             }
             if (ToLowerCase.IsChecked == true)
             {
                 Clipboard.SetText(TextBox.Text.ToLower());
+                var notification = new Notification("The text was successfully converted to lowercase and copied to the clipboard", Brushes.Lime);
+                notification.Margin = new Thickness(5,-90,0,-100);
+                notification.Style = (Style)FindResource("Notification");
+                Grid.SetRow(notification, 4);
+                MyGrid.Children.Add(notification);
                 return;
             }
 
